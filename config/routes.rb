b76f4182641852ccaca6eed40e117febc5f2980a
Shakeam <michalzygiel00@gmail.com>
2020-12-01 18:28:26 +0100

    scaffold init

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :posts, only: [:new, :create, :index]
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "users#index"
+end
